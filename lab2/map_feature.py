d2c87e976d11e4e71a5599180e213b79496c8eb3
Vũ Thành Công <88937753+thanhcong0111@users.noreply.github.com>
2024-06-18 09:23:13 +0700

    Add files via upload

---
+++ lab2/map_feature.py
@@ -0,0 +1,19 @@
+import numpy as np 
+
+def map_feature(x1, x2):
+#   x1, x2 type: numpy array
+#   Returns a new feature array with more features, comprising of 
+#   x1, x2, x1.^2, x2.^2, x1*x2, x1*x2.^2, etc.
+
+    degree = 6
+    out = np.ones([len(x1), int((degree + 1) * (degree + 2) / 2)])
+    idx = 1
+
+    for i in range(1, degree + 1):
+        for j in range(0, i + 1):
+            a1 = x1 ** (i - j)
+            a2 = x2 ** j
+            out[:, idx] = a1 * a2
+            idx += 1
+
+    return out
